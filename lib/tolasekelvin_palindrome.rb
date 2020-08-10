ef314e4d7a57d22ecaa4295065c72ead38088077
Klashe <officialklashed@gmail.com>
2020-08-10 16:07:21 +0100

    Initialize repository

---
+++ lib/tolasekelvin_palindrome.rb
@@ -0,0 +1,6 @@
+require "tolasekelvin_palindrome/version"
+
+module TolasekelvinPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tolasekelvin_palindrome/version.rb
@@ -0,0 +1,3 @@
+module TolasekelvinPalindrome
+  VERSION = "0.1.0"
+end
